915bab2c05b5f8003bcac081f7b30fd1a486f926
BMB <bruno.marquez@ucb.edu.bo>
2022-03-10 12:27:21 -0400

    Se agrega limite de caracteres

--- src/presenter.js
+++ src/presenter.js
@@ -10,12 +10,30 @@ post_form.addEventListener("submit", (event) => {
 
   const titulo_post = titulo.value;
   const detalle_post = detalle.value;
-
-  div_form.innerHTML = "<p>" + post_formatter(titulo_post, detalle_post) + "</p>";
+  if(characters(titulo_post, 50) == 1)
+  {
+    div_form.innerHTML = "<p>" + post_formatter(titulo_post, detalle_post) + "</p>";
+  }
+  else 
+  {
+    div_form.innerHTML = "<p>" +  + "No es posible publicar un post con mas de 50 caracteres" + "</p>";
+  }
 });
 
 function post_formatter(titulo, detalle)
 {
   const post = "========" + "<br>" + "Titulo: " + titulo + "<br>" +  "---------" +"<br>" + detalle + "<br>" + "========";
   return post;
+}
+
+function characters(text, number)
+{
+  if (text.lenght > number)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  } 
 }
